851953c490919dedfe6624a50c56bf8f828cc8a5
louis.medo <louis@loutik.fr>
2026-03-21 11:30:30 +0100

    feat: ajout du script boostrap

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,5 +1,6 @@
     </main>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <footer>
         <p>&copy; <?php echo $nom; ?> - <?php echo date('Y'); ?>
         <a href="mailto:<?php echo $email; ?>"Email></a></p>

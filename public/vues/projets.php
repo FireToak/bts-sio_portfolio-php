afed878e76153757b74dde010f9d0b34fe4591b9
louis.medo <louis@loutik.fr>
2026-03-28 10:55:39 +0100

    feat: ajout de la possibilité de cacher les projets non terminés et de compter ces derniers

--- public/vues/projets.php
+++ public/vues/projets.php
@@ -25,26 +25,39 @@
 
                         $projets = json_decode($jsonTexte, true);
 
+                        // Variable pour compter les projets non terminées
+                        $projet_non_termines = 0;
+
                         foreach ($projets as $projet) {
-                            echo "<div class='card overflow-hidden shadow rounded-4 border-0 mb-5'>";
-                            echo "<div class='ratio project-banner-wrap'>";
-                            echo "<img class='w-100 h-100 project-banner' src='" . $projet["image"] . "' alt='Bannière du projet' />";
-                            echo "</div>";
-                            echo "<div class='card-body p-4 p-md-5'>";
-                            echo "<h2 class='fw-bolder'>" . $projet['titre'] . "</h2>";
-                            echo "<p class='mb-0'>" . $projet['description'] . "</p>";
-                            echo "<h5 class='fw-bolder mt-3'>Compétences</h5>";
-                            if (!empty($projet['competences']) && is_array($projet['competences'])) {
-                            echo "<ul class='mb-3'>";
-                            foreach ($projet['competences'] as $competence) {
+                            if ($projet['termine'] == "true") {
+                                echo "<div class='card overflow-hidden shadow rounded-4 border-0 mb-5'>";
+                                echo "<div class='ratio project-banner-wrap'>";
+                                echo "<img class='w-100 h-100 project-banner' src='" . $projet["image"] . "' alt='Bannière du projet' />";
+                                echo "</div>";
+                                echo "<div class='card-body p-4 p-md-5'>";
+                                echo "<h2 class='fw-bolder'>" . $projet['titre'] . "</h2>";
+                                echo "<p class='mb-0'>" . $projet['description'] . "</p>";
+                                echo "<h5 class='fw-bolder mt-3'>Compétences</h5>";
+                                if (!empty($projet['competences']) && is_array($projet['competences'])) {
+                                echo "<ul class='mb-3'>";
+                                foreach ($projet['competences'] as $competence) {
                                 echo "<li>" . $competence . "</li>";
+                                }
+                                echo "</ul>";
+                                }
+                                echo "<a class='btn btn-primary' href='" . $projet['lien'] . "'>Découvrir</a>";
+                                echo "</div>";
+                                echo "</div>";
                             }
-                            echo "</ul>";
+                            else {
+                                $projet_non_termines += 1;
                             }
-                            echo "<a class='btn btn-primary' href='" . $projet['lien'] . "'>Découvrir</a>";
-                            echo "</div>";
-                            echo "</div>";
                         }
+                        echo "<div class='card border-info overflow-hidden shadow rounded-4 border-1 mb-5'>";
+                        echo "<div class='card-body p-4 p-md-5'>";
+                        echo "<h2 class='card-title text-center'> Il y a " . $projet_non_termines . " projet(s) inachevé(s).</h2>";
+                        echo "</div>";
+                        echo "</div>"
                      ?>
                 </div>
             </div>
